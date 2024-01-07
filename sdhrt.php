fbf4816e525eec45d0f3b4603c7debe45d8dc36e
kodhwu53 <138618485+kodhwu53@users.noreply.github.com>
2024-01-07 19:00:18 -0300

    Update sdhrt.php

--- sdhrt.php
+++ sdhrt.php
@@ -18,7 +18,7 @@ session_start();
 <?php
 // Set session variables
 $_SESSION["favcolor"] = "green";
-$_SESSION["favanimal"] = "cat";
+$_SESSION["favanimal"] = "catss";
 echo "Session variables are set.";
 ?>
 
