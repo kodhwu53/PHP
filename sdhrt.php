938c080757744e3639070cfc60bae185b712be66
kodhwu53 <138618485+kodhwu53@users.noreply.github.com>
2024-01-23 13:43:31 -0300

    Update sdhrt.php

--- sdhrt.php
+++ sdhrt.php
@@ -1,5 +1,5 @@
 <?php
-$myfile = fopen("newfile.txt", "wop") or die("Unable t oefle!");
+$myfile = fopen("newfile.txt", "wop") or die("Unable t oele!");
 $txt = "John Dooee\n";
 fwrite($myfile, $txt);
 $txt = "Janwe Doen";
